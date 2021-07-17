69e0dd31ce65563e5b8d236e7413a46806c219f1
Sanghyuk <saanghyuk@gmail.com>
2021-07-17 19:50:57 +0800

    20210717 - 1

--- programming_essentials_AN6100/M_part.py
+++ programming_essentials_AN6100/M_part.py
@@ -2,24 +2,27 @@ import os
 import pandas as pd
 import calendar
 
-# year_month = input("Please enter MONTH and YEAR : YYYYMM")
-year_month = '202105'
+# When you start service, please use the second line below -> # year_month = '202105'
+year_month = input("Please enter MONTH and YEAR : YYYYMM")
+# year_month = '202105'
 
 # get names list of all files
 file_list = os.listdir('./INOUT_forStudents')
 to_extract_file_list = [file for file in file_list if year_month in file]
 print(to_extract_file_list)
 
+# extract year and month -> str
 year = year_month[:4]
 month = year_month[-2:]
-print(year)
-print(month)
-#get the last day of the input month
+
+# get the last day of the input month
 last_day_of_month = calendar.monthrange(int(year), int(month))[-1]
 
+# Make the blank DF to append datas in the for loop below
 final_df = pd.DataFrame(columns=["Date", "In Time", "Out Time", "Token ID"])
 print(final_df)
 
+# for loop to extract, merge, append
 for i in range(last_day_of_month):
     try:
         day = i+1
@@ -40,14 +43,17 @@ for i in range(last_day_of_month):
     except:
         pass
 
+# to calculate the Hrs and Mins columns using DATETIME, DATEDELTA
 InTime_Array = pd.to_datetime(final_df["Out Time"])
 OutTime_Array = pd.to_datetime(final_df["In Time"])
 time_diff = InTime_Array - OutTime_Array
 hours = [time_diff[i].seconds//3600 for i in range(len(time_diff))]
 minutes = [(time_diff[i].seconds//60)%60 for i in range(len(time_diff))]
 
+# make new columns with the Series of hours and minues below
 final_df["Hrs"] = hours
 final_df["Mins"] = minutes
 
+# put out
 output_filename = "MG"+year+month+".csv"
 final_df.to_csv(output_filename)
